838c6d8fd1feeaea514439db0f1266518028463f
Benjamín Vateha <benjamin.vateha@student.tuke.sk>
2026-02-06 10:59:22 +0100

    monochromic + scene adjustments

---
+++ Assets/Scripts/Cards/TimeCard.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class BombCard : ItemCard
+{
+    public override void Use()
+    {
+        Debug.Log("⏱️ Time stopped!");
+        // sem neskôr dáš reálnu bomb logiku
+    }
+}
